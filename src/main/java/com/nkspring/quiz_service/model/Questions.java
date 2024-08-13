c62256000c68ea2c3bcc7ff53a516ece6ab3499e
Nantha <nanthu1593@gmail.com>
2024-08-13 23:55:02 +0800

    Import fix

---
+++ src/main/java/com/nkspring/quiz_service/model/Questions.java
@@ -0,0 +1,27 @@
+package com.nkspring.quiz_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public   class Questions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String category;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String question;
+    private String rightChoice;
+    private String difficultyLevel;
+
+
+
+
+}
