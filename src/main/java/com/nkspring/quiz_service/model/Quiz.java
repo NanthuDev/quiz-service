5936dd509621e4b7b20082611ae549d8283b5420
Nantha <nanthu1593@gmail.com>
2024-08-16 00:24:52 +0800

    Update Quiz.java

--- src/main/java/com/nkspring/quiz_service/model/Quiz.java
+++ src/main/java/com/nkspring/quiz_service/model/Quiz.java
@@ -13,6 +13,7 @@ public class Quiz {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
-    @ManyToMany
-    private List<Questions> questions;
+
+    @ElementCollection
+    private List<Integer> questionIds;
 }
