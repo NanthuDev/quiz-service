7b44ef587036c9331915b70b316ca801b759dd5e
Nantha <nanthu1593@gmail.com>
2024-08-17 22:58:29 +0800

    Update QuizInterface.java

--- src/main/java/com/nkspring/quiz_service/feign/QuizInterface.java
+++ src/main/java/com/nkspring/quiz_service/feign/QuizInterface.java
@@ -1,8 +1,25 @@
 package com.nkspring.quiz_service.feign;
 
 
+import com.nkspring.quiz_service.model.QuestionWrapper;
+import com.nkspring.quiz_service.model.Response;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient("QUESTION-SERVICE")
 public interface QuizInterface {
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(@RequestParam String catName, @RequestParam Integer numOfQuestions);
+
+    @PostMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds);
+
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses);
 }
